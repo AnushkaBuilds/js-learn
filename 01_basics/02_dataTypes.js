953bb65fead722dae37e977e3b689a184edac2a6
AnushkaBuilds <anushkapurohit10621@gmail.com>
2025-03-08 17:32:25 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3+3) //reference error, we are using nodejs, not browser
+
+/*
+javascript ka engine browser k andar chupa hua hai 
+dusre platforms pr html document ya developers console mein sikhate hai jahan alert ka pop-up display ho jata hai
+yaha aisa nahi hoga 
+*/
+
+console.log(3+
+    3)           //code readibility
+console.log("Anushka")  
+
+let name = "Anushka"   //string
+let age = 18           //integer
+let isLoggedIn = false //boolean
+let state = null
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object 
+
+console.log(typeof "Anushka")
+console.log(typeof null)          //objects
+console.log(typeof undefined)     //undefined
+
+/*
+Both null and undefined represent the absence of a value, but they are used in different situations.
+A variable that has been declared but has not been assigned a value.
+null is an intentional absence of a value, explicitly assigned by the developer.
+*/
