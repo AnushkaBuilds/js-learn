2964451cbbec8ab67bb88402089e6ead129cee76
AnushkaBuilds <anushkapurohit10621@gmail.com>
2025-03-09 14:19:12 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -36,3 +36,12 @@ console.log(Number("42px"))          //NaN
 
 
 //*****************************************operations****************************************//
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1)
