8b3cce39fcd369f8aa45bd26b323458985b80db8
AnushkaBuilds <anushkapurohit10621@gmail.com>
2025-03-11 14:42:20 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+/*
+const score = 100
+const balance = new Number(400)
+console.log(score)
+console.log(balance)
+console.log(score === balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+*/
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//console.log(Math)
+//console.log(Math.round(4.6))
+//console.log(Math.ceil(4.3))
+//console.log(Math.floor(4.9))
+//console.log(Math.pow(2,3))
+//console.log(Math.sqrt(81))
+//console.log(Math.min(4.6897,4.6899))
+//console.log(Math.max(4.6897,4.6899))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
