0c79f2186f97274b334282d18d7c4c07bb4aa7b8
AnushkaBuilds <anushkapurohit10621@gmail.com>
2025-03-11 18:29:41 +0000

    dates in javascript

---
+++ 01_basics/07_datesInJS.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+
+// myCreatedDate = new Date(2023,0,29)
+let myCreatedDate = new Date(2003,0,29,5,3)
+console.log(myCreatedDate.toLocaleString())
