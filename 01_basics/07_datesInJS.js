88aa10c6db82210998f3a79292d9d844be9404e8
AnushkaBuilds <anushkapurohit10621@gmail.com>
2025-03-12 06:12:38 +0000

    date and time in javascript

--- 01_basics/07_datesInJS.js
+++ 01_basics/07_datesInJS.js
@@ -6,3 +6,20 @@ console.log(myDate.toLocaleString())
 // myCreatedDate = new Date(2023,0,29)
 let myCreatedDate = new Date(2003,0,29,5,3)
 console.log(myCreatedDate.toLocaleString())
+
+
+let newdate = new Date(2009,00,17,12,33)
+console.log(newdate.toString())
+console.log(newdate.toDateString())
+console.log(newdate.toLocaleString())
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++ TIME +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*let myTimeStamp = Date.now()
+console.log(newdate.getTime())
+
+console.log(Math.floor(Date.now()/1000))*/
+
+let newdate1 = new Date()
+console.log(newdate1)
+console.log(newdate1.getMonth()+1)
